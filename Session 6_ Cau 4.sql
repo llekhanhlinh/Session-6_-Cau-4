d74c112c784b63732c4cdc59eff16d63adb53158
llekhanhlinh <llekhanhlinh29@gmail.com>
2025-11-17 08:10:05 +0700

    Add files via upload

---
+++ Session 6_ Cau 4.sql
@@ -0,0 +1,48 @@
+create database QuanLyThuVien;
+use QuanLyThuVien;
+
+create table TacGia(
+	MaTacGia int primary key auto_increment,
+    TenTacGia varchar(100) not null,
+    QuocTich varchar(50)
+);
+    
+create table DocGia(
+	MaDocGia int primary key auto_increment,
+    TenDocGia varchar(100) not null,
+    DiaChi varchar(50),
+    SoDienThoai varchar(15) 
+);
+
+select distinct SoDienThoai
+from DocGia;
+
+insert into TacGia(TenTacGia, QuocTich)
+values ('Nguyễn Nhật Ánh', 'Việt Nam'),
+('J.K. Rowling', 'Anh'),
+('Haruki Murakami', 'Nhật Bản'),
+('Dale Carnegie', 'Mỹ');
+
+insert into DocGia(TenDocGia, DiaChi, SoDienThoai)
+values ('Nguyễn Văn A', '123 Đường ABC, Hà Nội', '0901234567'),
+('Trần Thị B', '456 Đường XYZ, TP.HCM', '0912345678'),
+('Lê Văn C', '789 Đường LMN, Đà Nẵng', '0923456789'),
+('Phạm Thị D', '101 Đường QRS, Hà Nội', '0934567890'),
+('Hoàng Văn E', '202 Đường UVW, Cần Thơ', '0945678901');
+
+select * from TacGia
+where QuocTich like 'Việt Nam';
+
+select * from DocGia
+where DiaChi like '%Hà Nội%';
+
+select TenDocGia, SoDienThoai
+from DocGia;
+
+update DocGia
+set DiaChi = '200 Đường XYZ, TP. Thủ Đức'
+where MaDocGia = 2;
+
+DELETE FROM TacGia
+WHERE MaTacGia = 3;
+
